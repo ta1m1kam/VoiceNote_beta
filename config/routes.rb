9d7077a731302b31714af4854e3ed0d967b67d31
TaigaMikami <mikamimikami60@gmail.com>
2017-11-02 17:53:33 +0900

    users_controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get '/home' => 'static_pages#home'
+  get '/signup' => 'users#new'
+
+  root 'static_pages#home'  # get '/' => 'static_pages#home'
   get '/about' => 'static_pages#about'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/contact' => 'static_pages#contact'
+
 end
