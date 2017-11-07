d5c9bec2eeadc22a0688c0d801eeb11f01497720
TaigaMikami <mikamimikami60@gmail.com>
2017-11-08 01:03:18 +0900

    session実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user #redirect_to user_url(@user)と等価
+      redirect_to @user #redirect_to user_url(@user)と等価 /:id/showページ
     else
       render 'new'
     end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
 
   get '/signup' => 'users#new'
   resources :users
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
 end
