a2746b850af5ab26805c13006fa91b73d0ea1de5
TaigaMikami <mikamimikami60@gmail.com>
2017-11-09 00:37:58 +0900

    cookie

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,16 +5,39 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def remember
+    user.remember
+    cookies.parmanent.signed[:user_id] = user.id
+    cookies.parmanent[:remember_token] = user.remember_token ##20年で切れるCookiesの設定
+  end
+
   # 現在ログイン中のユーザーを返す (いる場合)
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id])
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   def logged_in?
     !current_user.nil?
   end
 
+  # 永続的セッションを破棄する
+ def forget(user)
+   user.forget
+   cookies.delete(:user_id)
+   cookies.delete(:remember_token)
+ end
+
+
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+  before_save { self.email = email.downcase }
   validates :name, {presence: true, length: {maximum: 50}}
   validates :email, {presence: true}
   has_secure_password
@@ -6,4 +8,32 @@ class User < ApplicationRecord
 
   include Gravtastic
   gravtastic
+
+  # 渡された文字列のハッシュ値を返す
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.degest(remember_token))
+  end
+
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # ユーザーのログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
